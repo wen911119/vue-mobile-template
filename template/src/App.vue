a39b9ae377a0ec3d6a667b73d7acbdb3240da36e
wen911119 <wen911119@gmail.com>
2017-12-19 22:36:23 +0800

    样式部分

--- template/src/App.vue
+++ template/src/App.vue
@@ -22,13 +22,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+@font-face {
+    font-family: 'iconfont';
+    src:url('./assets/iconfont.ttf') format('truetype')
 }
 </style>
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+const demo1 = ()=>import('@/modules/Demo/pages/demo1/app.vue')
+const demo2 = ()=>import('@/modules/Demo/pages/demo2/app.vue')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/demo1',
+      name: 'demo1',
+      component: demo1
+    },
+    {
+        path: '/demo2',
+        name: 'demo2',
+        component: demo2
+      },
+    { path: '*', redirect: '/demo1' }
   ]
 })
