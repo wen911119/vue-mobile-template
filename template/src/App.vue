3769393a887058adb3715a1e3fe6a37f407a4895
wen911119 <wen911119@gmail.com>
2017-12-19 23:37:37 +0800

    modify

--- template/src/App.vue
+++ template/src/App.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import './style/common.scss';
 @font-face {
     font-family: 'iconfont';
     src:url('./assets/iconfont.ttf') format('truetype')
