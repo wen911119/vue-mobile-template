b43c4fd33bda8a72b5a6568f3de653e64818ef70
wen911119 <wen911119@gmail.com>
2017-12-19 13:18:23 +0800

    目录约定示例

---
+++ template/src/modules/Demo/pages/demo1/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login-page">
+        demo1
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+<style>
+    
+</style>
---
+++ template/src/modules/Demo/pages/demo2/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login-page">
+        demo2
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+<style>
+    
+</style>
