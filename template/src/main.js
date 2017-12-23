baf857b364c5ca44abe9e53c8ce86a7e905979fc
wen911119 <wen911119@gmail.com>
2017-12-23 15:59:24 +0800

    增加i18n

--- template/src/main.js
+++ template/src/main.js
@@ -11,6 +11,18 @@ if (window.__INITIAL_STATE__) {
 {{#router}}
 import router from './router'
 {{/router}}
+
+{{#i18n}}
+import VueI18n from 'vue-i18n'
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+    locale: 'zh-CN', // 默认中文
+    messages: {
+        'zh-CN': require('./i18n/lang/zh-CN.json'), // 中文语言包
+        'en-US': require('./i18n/lang/en-US.json') // 英文语言包
+    }
+})
+{{/i18n}}
 Vue.config.devTools = true
 Vue.config.productionTip = false
 
@@ -21,6 +33,9 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
+  {{#i18n}}
+  i18n,
+  {{/i18n}}
   {{#if_eq build "runtime"}}
   render: h => h(App)
   {{/if_eq}}
