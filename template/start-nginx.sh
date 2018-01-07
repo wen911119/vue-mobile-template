2fdb6723900f1663205c5b855d81e618612495d7
wen911119 <wen911119@gmail.com>
2018-01-07 18:22:43 +0800

    ci配置文件修改

--- template/start-nginx.sh
+++ template/start-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /bin/sed -i "s/<api_host_placeholder>/${API_SERVER}/" /etc/nginx/conf.d/default.conf && \
 /bin/sed -i "s/<isv_host_placeholder>/${ISV_SERVER}/" /etc/nginx/conf.d/default.conf && \
-nginx -g 'daemon off;
+nginx -g 'daemon off;'
