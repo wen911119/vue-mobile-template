1f0734b8fe30f449c346ed2ffd0fe1fde5b93ac1
wen911119 <wen911119@gmail.com>
2018-01-15 09:23:47 +0800

    update 环境检测的utils

--- template/src/utils/env.js
+++ template/src/utils/env.js
@@ -1,9 +1,9 @@
 export function isIOS(){
-
+    return /(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)
 }
 
 export function isAndroid(){
-
+    return /(Android)/i.test(navigator.userAgent)
 }
 
 export function isWeb(){
