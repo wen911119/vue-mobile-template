c65bfd500d8927204e961a9fab516630459207fc
wen911119 <wen911119@gmail.com>
2018-01-15 09:25:21 +0800

    update network.js

--- template/src/interfaces/network.js
+++ template/src/interfaces/network.js
@@ -3,8 +3,22 @@ import { LOADING } from '@/interfaces/indicator'
 import axios from 'axios'
 const base_url = process.env.API_URL
 
-export function GET() {
-
+export function GET(url, params = {}, loading = true) {
+    if (isWeb()) {
+        url = base_url + url
+        return new Promise((resolve, reject) => {
+            loading && LOADING('show')
+            axios.get(url, { params }).then(res => {
+                LOADING('hide')
+                resolve(res)
+            }).catch(err => {
+                LOADING('hide')
+                reject(err)
+            })
+        })
+    } else {
+        // todo 其它环境的实现
+    }
 }
 
 export function POST(url, params = {}, loading = true) {
